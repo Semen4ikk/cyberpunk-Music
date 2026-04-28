b48aa3bda4b4b6213212109c93773071eabe0a83
Semen Prokopets <semen2004vuz@yandex.ru>
2026-04-28 21:29:49 +0400

    настроил навигацию

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {Playlist} from './features/playlist/pages/playlist';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'playlist/:slug', component: Playlist}
+];
