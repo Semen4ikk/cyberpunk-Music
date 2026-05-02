3cc5820d2ec0b0250944ec828227ed87b2766794
Semen Prokopets <semen2004vuz@yandex.ru>
2026-05-02 17:54:56 +0400

    добавил навигацию

--- frontend/src/app/features/playlist/nav/playlist-nav/playlist-nav.ts
+++ frontend/src/app/features/playlist/nav/playlist-nav/playlist-nav.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {PLAYLIST} from '../../../../core/data/playlist.data';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-playlist-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './playlist-nav.html',
   styleUrl: './playlist-nav.scss',
 })
