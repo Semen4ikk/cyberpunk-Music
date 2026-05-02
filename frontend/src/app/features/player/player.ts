f5e9f9064930e7a36f4588aa9c15e442eb729aad
Semen Prokopets <semen2004vuz@yandex.ru>
2026-05-02 17:54:03 +0400

    добавил плеер

--- frontend/src/app/features/player/player.ts
+++ frontend/src/app/features/player/player.ts
@@ -1,4 +1,6 @@
-import {Component, signal} from '@angular/core';
+import {Component, effect, ElementRef, input, output, signal, ViewChild} from '@angular/core';
+import {TrackModel} from '../../core/models/track.model';
+import {TRACKS} from '../../core/data/tracks.data';
 
 @Component({
   selector: 'app-player',
@@ -7,5 +9,59 @@ import {Component, signal} from '@angular/core';
   styleUrl: './player.scss',
 })
 export class Player {
-  protected readonly title = signal('Player');
+  @ViewChild('audioElement') audioRef!: ElementRef<HTMLAudioElement>;
+
+  handleSelectNextTrack = output<void>();
+  handleSelectPreviousTrack = output<void>();
+
+  currentTrack = input<TrackModel | null>(TRACKS[0]);
+  isPlaying = signal(false)
+  currentTime = signal(0)
+
+  constructor() {
+    effect(()=>{
+      const track = this.currentTrack();
+      if(track && this.audioRef){
+        this.audioRef.nativeElement.load()
+        if(this.isPlaying()){
+          this.audioRef.nativeElement.play();
+        }
+      }
+    })
+  }
+
+
+
+  get formattedTime() : string {
+    const time = this.currentTime()
+    const minutes = Math.floor(time / 60)
+    const seconds = Math.floor(time % 60)
+    return `${minutes.toString().padStart(2, '0')}:
+    ${seconds.toString().padStart(2, '0')}:${Math.floor((time % 1)* 100)
+      .toString().padStart(2, '0')}`
+  }
+
+  togglePlay(){
+    if (this.isPlaying()){
+      this.audioRef.nativeElement.pause();
+    }else{
+      this.audioRef.nativeElement.play();
+    }
+    this.isPlaying.set(!this.isPlaying())
+  }
+
+  previous(){
+    this.handleSelectPreviousTrack.emit()
+  }
+  next(){
+    this.handleSelectNextTrack.emit()
+  }
+  rewind(){
+    this.audioRef.nativeElement.currentTime -=10
+  }
+  forward(){
+    this.audioRef.nativeElement.currentTime +=10
+  }
+
+  protected readonly ontimeupdate = ontimeupdate;
 }
